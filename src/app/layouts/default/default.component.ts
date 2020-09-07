1314445a9f037d1a7108f0a4f0a3e52b51fa9bb2
Yossi Ben Zaken <yossibz@gmail.com>
2020-09-07 10:13:01 +0300

    first commit

---
+++ src/app/layouts/default/default.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-default',
+  templateUrl: './default.component.html',
+  styleUrls: ['./default.component.scss']
+})
+export class DefaultComponent {
+  sideBarOpen = true;
+  sideBarToggler(e) {
+    this.sideBarOpen = !this.sideBarOpen;
+  }
+}
---
+++ src/app/layouts/default/default.module.ts
@@ -0,0 +1,37 @@
+import { DashboardService } from './../../modules/dashboard.service';
+import { PostsComponent } from './../../modules/posts/posts.component';
+import { SharedModule } from './../../shared/shared.module';
+import { RouterModule } from '@angular/router';
+import { DashboardComponent } from './../../modules/dashboard/dashboard.component';
+import { DefaultComponent } from './default.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
+
+@NgModule({
+  declarations: [
+    DefaultComponent,
+    DashboardComponent,
+    PostsComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SharedModule,
+    MatSidenavModule,
+    MatDividerModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatPaginatorModule,
+    MatTableModule
+  ],
+  providers: [DashboardService]
+})
+export class DefaultModule { }
